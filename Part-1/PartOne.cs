009d9cf58eef84eec7d972b844676b60f20c9f89
vasilijus <vasiliokas@gmail.com>
2023-05-28 15:45:52 +0100

    Selection App fix

--- Part-1/PartOne.cs
+++ Part-1/PartOne.cs
@@ -1,24 +1,20 @@
 ﻿using System;
-using App1.Part_1;
 using App1.Part_1.Progi;
 using App1.Part_1.Games.ManticoreHunt;
 namespace App1;
 
-class Program {
-    public static void Main(string[] args) {
+class PartOne {
+    public void Selection() {
 
         Console.WriteLine("Enter Number: .. ");
         Console.WriteLine("\n ");
-        Console.WriteLine("From 1-4 ");
-
         int selection = Convert.ToInt32(Console.ReadLine());
 
         switch (selection)
         {
             case 1:
-                Console.WriteLine("Part 1 - The Basics");
-                PartOne p1Basics = new PartOne();
-                p1Basics.Selection();
+                Console.WriteLine("App 1");
+                // GridProtect.Init();
                 break;
             
             case 2:
@@ -33,6 +29,22 @@ class Program {
             case 4:
                 Console.WriteLine("App 4");
                 BuyingInventory.Init();
+                break;
+            case 5:
+                Console.WriteLine("App 5");
+                // ReplicatorOfDto.Init();
+                break;
+            case 6:
+                Console.WriteLine("App 6");
+                TakingANumber.Start();
+                break;
+            case 7:
+                Console.WriteLine("App 7");
+                // Countdown.Init();
+                break;
+            case 8:
+                Console.WriteLine("App 8");
+                HuntingManticore.Init();
                 break;
                 
             default:
