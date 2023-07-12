b09484c0c4ca86530fe5bd5ceacbd2af11e9ea14
vasilijus <vasiliokas@gmail.com>
2023-07-12 23:46:31 +0100

    fix RPC

--- Part-2/Games/RockPaperScissors/RockPaperScissors.cs
+++ Part-2/Games/RockPaperScissors/RockPaperScissors.cs
@@ -3,31 +3,53 @@ namespace App1.Part_2.Games;
 class RockPaperScissors
 {
     private int player = 1;
+    private int games = 0, won1 = 0, won2 = 0;
 
     public void Draw()
     {
-        Console.WriteLine($"1.Rock, 2.Paper, 3.Scissor");
-        Console.WriteLine($"Player {player}:");
-        var p1 = int.Parse(Console.ReadLine());
-        var p2 = int.Parse(Console.ReadLine());
-        Challenge(p1, p2);
+        while (true)
+        {
+            Hand p1 = GetHand(1);
+            Hand p2 = GetHand(2);
+
+            int won = Challenge(p1, p2);
+            // Count Player scores
+            games++;
+            if (won == 1) won1++;
+            if (won == 2) won2++;
+            Console.WriteLine($"Player {won} WON. Total Games: {games}, 1st {won1}, 2nd {won2}");
+        }
     }
-    public void Challenge(int p1, int p2)
+
+    private Hand? GetHand(int player)
+    {
+        Console.WriteLine($"Player {player} turn.");
+        Console.WriteLine($"1.Rock, 2.Paper, 3.Scissor");
+        
+        int number = 0;
+        Hand hand = Hand.Paper;
+        while(number < 1 || number > 3)
+        {
+            number = int.Parse(Console.ReadLine());
+            hand = number switch
+            {
+                1 => Hand.Rock,
+                2 => Hand.Paper,
+                3 => Hand.Scissor,
+                _ => Hand.Paper
+            };
+        }
+        return hand;
+    }
+
+    public int Challenge(Hand p1, Hand p2)
     {
         // rock bet scissor
-        if(p1 == 1 && p2 == 2){
-            Console.WriteLine($"Player player 2 - won ");
-        }
-        else if(p1 == 2 && p2 == 3){
-            Console.WriteLine($"Player player 2 - won ");
-        }
-        else if(p1 == 3 && p2 == 1){
-            Console.WriteLine($"Player player 2 - won ");
-        }
-        else
-        {
-            Console.WriteLine($"Player player 1 - won ");
-        }
+        if(p1 == Hand.Rock && p2 == Hand.Paper) return 2;
+        else if(p1 == Hand.Paper && p2 == Hand.Scissor) return 2;
+        else if(p1 == Hand.Scissor && p2 == Hand.Rock) return 2;
+        else 
+            return 1;
     }
 
     
